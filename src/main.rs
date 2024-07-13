9375305e974be195eccb8d44f8ff2cb3262c53da
Famoto <25197520+Famoto@users.noreply.github.com>
2024-07-13 15:36:26 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -41,11 +41,8 @@ fn main() -> anyhow::Result<()> {
 			object_hash,
 		} => {
 			anyhow::ensure!(
-
                 pretty_print,
-
                 "mode must be given without -p, and we don't support mode"
-
             );
 
 			// TODO: support shortest-unique object hashes
@@ -55,37 +52,32 @@ fn main() -> anyhow::Result<()> {
 				&object_hash[..2],
 				&object_hash[2..]
 			))
-
 				.context("open in .git/objects")?;
+
 			let z = ZlibDecoder::new(f);
 			let mut z = BufReader::new(z);
 			let mut buf = Vec::new();
 			z.read_until(0, &mut buf).context("read header from .git/objects")?;
 
-			let header = CStr::from_bytes_with_nul(&buf).expect("know there is exactly one nul, and it's at the end");
+			let header = CStr::from_bytes_with_nul(&buf)
+				.expect("know there is exactly one nul, and it's at the end");
 
 			let header = header.to_str().context(".git/objects file header isn't valid UTF-8")?;
 
-			let Some((kind, size)) = header.split_once(' ') else {
+			let Some((size)) = header.strip_prefix("blob ") else {
 				anyhow::bail!(
                     ".git/objects file header did not start with a known type: '{header}'"
                 );
 			};
 
-			let kind = match kind {
-				"blob" => Kind::Blob,
-				_ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
-			};
-
 			let size = size.parse::<usize>().context(".git/objects file header has invalid size: {size}")?;
-
 			buf.clear();
 			buf.resize(size, 0);
 			z.read_exact(&mut buf[..]).context("read true contents of .git/objects file")?;
 
 			let n = z.read(&mut [0]).context("validate EOF in .git/object file")?;
-
 			anyhow::ensure!(n == 0, ".git/object file had {n} trailing bytes");
+
 			let stdout = std::io::stdout();
 			let mut stdout = stdout.lock();
 			match kind {
@@ -93,6 +85,5 @@ fn main() -> anyhow::Result<()> {
 			}
 		}
 	}
-
 	Ok(())
 }
